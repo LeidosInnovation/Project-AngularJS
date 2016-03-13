12ba64effb033ef9f5c989983af73d5da9ef1910
sgavathe <sgavathe@gmail.com>
2016-03-13 17:50:51 -0400

    updated with new controller, dialog, nvd3 good stuff.

--- AngularJS-02/App.js
+++ AngularJS-02/App.js
@@ -1,5 +1,5 @@
 // create the module and name it app
-var app = angular.module('angularjsApp', ['ngRoute']);
+var app = angular.module('angularjsApp', ['ngRoute', 'ui.bootstrap', 'nvd3']); //route-routing config, nvd3-chart, bootstrap ui-dialog 
 
 // configure our routes
 app.config(function ($routeProvider) {
@@ -23,4 +23,13 @@ app.config(function ($routeProvider) {
         .when('/custDirective', {
             templateUrl: 'customDirective.html'
         })
+
+      // route for the directive page
+        //Notice i did not pass a controller here
+        //it is assigned in the view
+        .when('/nvd3Chart', {
+            templateUrl: 'chart.html',
+            controller: 'chartController'
+        })
+    
 });
